64316de5aa8ac5a07ffe98be11d480541bb1c198
Abigail Robinson <abbyrobinson@shaw.ca>
2022-12-20 15:40:33 -0700

    added form to compnents

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import SignUp from "./components/SignUp.jsx";
-import { Container } from "react-bootstrap";
+import Form from "./components/common/Form.js";
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <SignUp />
-      </div>
-    </Container>
+    <div>
+      <Form />
+    </div>
   );
 }
 
