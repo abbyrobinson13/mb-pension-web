0cd152002b9e56fabee35e63283c5b9b180f1c07
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2022-12-19 18:13:22 -0700

    Backend connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,32 @@
-import express, { application } from "express";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = 5000;
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+// MONGODB SETUP
+dotenv.config();
+async function main() {
+  
+
+  await mongoose.connect(
+    process.env.MONGODB_URI,
+
+    {
+      dbName: process.env.MONGODB_DBNAME,
+
+      user: process.env.MONGODB_USER,
+
+      pass: process.env.MONGODB_PASSWORD,
+    }
+  );
+  console.log(`Connected to Database :  ${process.env.MONGODB_DBNAME}`)
+}
+
+main().catch((err) => console.log(err));
