57badae42b224599f8e11a2b6fd8de5e2d2dc062
Abigail Robinson <abbyrobinson@shaw.ca>
2022-12-21 11:38:39 -0700

    added props, button

--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Box, TextField } from "@mui/material";
+import SimpleButton from "./Button.js";
 
-export default function BasicTextFields() {
+export default function Form({ title, setPassword, setEmail, handleAction }) {
   return (
     <div>
       <div className="heading-container">
-        <h3>Login Form</h3>
+        <h3>{title} Form</h3>
       </div>
 
       <Box
@@ -16,13 +17,21 @@ export default function BasicTextFields() {
         noValidate
         autoComplete="off"
       >
-        <TextField id="email" label="Enter the Email" variant="outlined" />
+        <TextField
+          id="email"
+          label="Enter the Email"
+          variant="outlined"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <TextField
           id="password"
           label="Enter the Password"
           variant="outlined"
+          onChange={(e) => setPassword(e.target.value)}
         />
       </Box>
+
+      <SimpleButton title={title} handleAction={handleAction} />
     </div>
   );
 }
