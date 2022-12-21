2ce7876c44740fa04feb8ea6e49daab908a0c92c
Abigail Robinson <abbyrobinson@shaw.ca>
2022-12-21 11:38:12 -0700

    Added routing and pages for registration and login.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,100 @@
 import React from "react";
+import "./App.css";
 import Form from "./components/common/Form.js";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { app } from "./firebase-config.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import Home from "./components/Home.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+//router added at root level so in can be used in the entire application
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    let authToken = sessionStorage.getItem("Auth Token");
+
+    if (authToken) {
+      navigate("/home");
+    }
+  }, []);
+
+  const handleAction = (id) => {
+    const authentication = getAuth();
+    if (id === 1) {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          if (error.code === "auth/wrong-password") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/user-not-found") {
+            toast.error("Please check the Email");
+          }
+        });
+    }
+    if (id === 2) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.code === "auth/email-already-in-use") {
+            toast.error("Email Already in Use");
+          }
+        });
+    }
+  };
+
   return (
-    <div>
-      <Form />
+    <div className="App">
+      <ToastContainer />
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <Form
+              title="Log In"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(1)}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Form
+              title="Register"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(2)}
+            />
+          }
+        />
+
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
