82ee2cfbb511cb24d7f4435827f06648f0f0b3fb
Tosin-Kasaba <tosinkasaba1@gmail.com>
2022-12-21 11:51:27 -0700

    refactor: renamed port variable

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 const app = express();
-const PORT = 5000;
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-// MONGODB SETUP
-dotenv.config();
-async function main() {
-  
-
-  await mongoose.connect(
-    process.env.MONGODB_URI,
-
-    {
-      dbName: process.env.MONGODB_DBNAME,
-
-      user: process.env.MONGODB_USER,
-
-      pass: process.env.MONGODB_PASSWORD,
-    }
-  );
-  console.log(`Connected to Database :  ${process.env.MONGODB_DBNAME}`)
-}
-
-main().catch((err) => console.log(err));
