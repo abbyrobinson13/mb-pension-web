a5457354cc854bed7a65db07a1fbd11efab87993
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2022-12-21 18:05:03 -0700

    db.js modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(
-      `MongoDB Connected: ${process.env.MONGODB_DBNAME}`.cyan.underline
+    mongoose.connect(process.env.MONGODB_URI, () =>
+      console.log(
+        `MongoDB Connected: ${process.env.MONGODB_URI}`.cyan.underline
+      )
     );
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold);
