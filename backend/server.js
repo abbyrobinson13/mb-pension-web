c72d27697e7013f28026f6d99de0d6585e8b3217
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2022-12-21 18:00:42 -0700

    server.js modified

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,31 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
 app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+// employee schema set up
+
+function main() {
+
+  const employeeSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+  });
+
+  const Employee = new mongoose.model("Employee", employeeSchema);
+
+  const employee = new Employee({
+    firstName: "Afshin",
+    lastName: "Sharifnia",
+    email: " afshin.sharifnia@gmail.com",
+  });
+  console.log("hello");
+  employee.save();
+}
+main();
