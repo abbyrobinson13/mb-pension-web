a958315c57b3681c6d1fff7a756bfde9be2edbb7
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2022-12-23 17:24:19 -0700

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import colors from "colors";
 
 const app = express();
+
+app.use(express.json());
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
 connectDB();
-
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-// employee schema set up
-
-function main() {
-
-  const employeeSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-  });
-
-  const Employee = new mongoose.model("Employee", employeeSchema);
-
-  // posting an employee object to MongoDB
-  const employee = new Employee({
-    firstName: "Afshin",
-    lastName: "Sharifnia",
-    email: " afshin.sharifnia@gmail.com",
-  });
-  console.log("Employee info sent to database");
-  employee.save();
-}
-main();
