db3f62ae87874daec62d5986e0993d30cef0352d
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2022-12-30 18:31:14 -0700

    added new fields to Schema

--- backend/src/models/employeeModels.js
+++ backend/src/models/employeeModels.js
@@ -9,10 +9,54 @@ const employeeSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please add a last name"],
   },
+  gender: {
+    type: String,
+    required: [true, "Please add a gender"],
+  },
+  birthDate: {
+    type: String,
+    required: [true, "Please add a birthdate"],
+  },
+  department: {
+    type: String,
+    required: [true, "Please add a department"],
+  },
+  position: {
+    type: String,
+    required: [true, "Please add a position"],
+  },
+  employmentDate: {
+    type: String,
+    required: [true, "Please add a start date of employment"],
+  },
+  dependents: {
+    type: String,
+    required: [true, "Please add the number of dependents"],
+  },
   email: {
     type: String,
     required: [true, "Please add an email"],
     unique: true,
+  },
+  mobile: {
+    type: String,
+    required: [true, "Please add a mobile number"],
+  },
+  street: {
+    type: String,
+    required: [true, "Please add a street name"],
+  },
+  postalCode: {
+    type: String,
+    required: [true, "Please add a postal code"],
+  },
+  city: {
+    type: String,
+    required: [true, "Please add a city"],
+  },
+  province: {
+    type: String,
+    required: [true, "Please add a province"],
   },
 });
 
