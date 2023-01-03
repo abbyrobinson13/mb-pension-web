d4724f921e13caff6d81e786b51edb43aa3d6531
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-02 22:51:41 -0700

    updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "../config/db.js";
 import { employeeRouter } from "./routes/employeeRoute.js";
+import { csvRouter } from "./routes/csvRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/employee", employeeRouter);
+app.use("/api/csv", csvRouter)
 
 dotenv.config();
 
