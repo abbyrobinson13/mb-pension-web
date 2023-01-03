c7b57081162e97eacf0551d9a8905fa27a7030ad
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-03 10:07:06 -0700

    doc: changes made to required fields.

--- backend/src/models/employeeModels.js
+++ backend/src/models/employeeModels.js
@@ -1,66 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const employeeSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, "Please add a name"],
+    required: [true, 'Please add a name']
   },
   lastName: {
     type: String,
-    required: [true, "Please add a last name"],
+    required: [true, 'Please add a last name']
   },
   gender: {
     type: String,
-    required: [true, "Please add a gender"],
+    required: [true, 'Please add a gender']
   },
   birthDate: {
     type: String,
-    required: [true, "Please add a birthdate"],
+    required: [true, 'Please add a birthdate']
   },
-  department: {
-    type: String,
-    required: [true, "Please add a department"],
-  },
-  position: {
-    type: String,
-    required: [true, "Please add a position"],
-  },
-  employmentDate: {
-    type: String,
-    required: [true, "Please add a start date of employment"],
-  },
-  dependents: {
-    type: String,
-    required: [true, "Please add the number of dependents"],
-  },
+
   email: {
     type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: [true, "Please add a mobile number"],
-  },
-  street: {
-    type: String,
-    required: [true, "Please add a street name"],
-  },
-  postalCode: {
-    type: String,
-    required: [true, "Please add a postal code"],
-  },
-  city: {
-    type: String,
-    required: [true, "Please add a city"],
-  },
-  province: {
-    type: String,
-    required: [true, "Please add a province"],
-  },
+    required: [true, 'Please add an email'],
+    unique: true
+  }
 });
 
-export const Employee = mongoose.model("Employee", employeeSchema);
+export const Employee = mongoose.model('Employee', employeeSchema);
 
 // create employee
 export const createEmployee = async (employee) => {
