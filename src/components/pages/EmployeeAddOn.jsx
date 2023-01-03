99fda334b041aad1f6497a2d38c4e1864de82585
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-02 19:58:33 -0700

    fix: fixed error

--- src/components/pages/EmployeeAddOn.jsx
+++ src/components/pages/EmployeeAddOn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const SuperheroForm = () => {
+const EmployeeAddOn = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [gender, setGender] = useState("");
@@ -150,4 +150,4 @@ const SuperheroForm = () => {
   );
 };
 
-export default SuperheroForm;
+export default EmployeeAddOn;
