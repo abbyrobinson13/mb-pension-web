29c525273cbdb82ecf3b0fa22258e64d3cbd00ba
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-02 22:49:15 -0700

    add csv route

---
+++ backend/src/routes/csvRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import csvtojson from "csvtojson";
+import { Employee } from "../models/employeeModels.js";
+
+export const csvRouter = express.Router();
+
+csvRouter.post("/add", async (req, res) => {
+  csvtojson()
+    .fromFile("EmployeeTemplateSample.csv")
+    .then((csvData) => {
+      console.log(csvData);
+      Employee.insertMany(csvData)
+        .then(function () {
+          console.log("Data inserted");
+          res.json({ success: "success" });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+});
