521d7b015feee22dd3e8201faf468e97d0ebf379
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-04 04:09:59 -0700

    doc: get an employee by ID

--- backend/src/routes/employeeRoute.js
+++ backend/src/routes/employeeRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createEmployee, getAllEmployees } from "../models/employeeModels.js";
+import { createEmployee, getAllEmployees, getEmployeeById } from "../models/employeeModels.js";
 
 export const employeeRouter = express.Router();
 
@@ -17,6 +17,20 @@ employeeRouter.get("/", async (req, res) => {
   try {
     const employees = await getAllEmployees();
     res.send(employees);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//identify an employee by ID
+employeeRouter.get("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const employee = await getEmployeeById();
+    if (!employee){
+      return res.status(404).send('Invalid employee id');
+    }
+    res.send(employee);
   } catch (error) {
     res.status(500).send(error);
   }
