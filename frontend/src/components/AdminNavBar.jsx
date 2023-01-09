0871b0df54f57e29e26a75e7c84b76b7da040d48
Abigail Robinson <abbyrobinson@shaw.ca>
2023-01-08 23:43:13 -0700

    created admin nav bar

---
+++ frontend/src/components/AdminNavBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function AdminNavBar() {
+  return (
+    <div className="Nav-Container">
+      <ul className="Page-Container">
+        <li className="Nav-Page">
+          <div className="dropdown push">
+            <NavLink to="/">
+              <span> Home </span>
+            </NavLink>
+          </div>
+        </li>
+        <li className="Nav-Page">
+          <div className="dropdown">
+            <NavLink to="/companies">
+              <span>View All Companies</span>
+            </NavLink>
+            <div className="dropdown-content">
+              <div className="first-dropdown">
+                <NavLink to="/employees">Upload New Employee CSV</NavLink>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="Nav-Page">
+          <div className="dropdown">
+            <NavLink to="/createcompany">
+              <span> Add New Company </span>
+            </NavLink>
+          </div>
+        </li>
+        <li className="Nav-Page">
+          <div className="dropdown">
+            <NavLink to="/profile">
+              <span> Profile </span>
+            </NavLink>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default AdminNavBar;
