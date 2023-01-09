632b125a63e3f47df45a511d6bb940c425cfc3f4
Abigail Robinson <abbyrobinson@shaw.ca>
2023-01-08 20:31:31 -0700

    added company list import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import Employees from './components/pages/employees.jsx';
 import EmployeeList from './components/pages/EmployeeList.jsx';
 import EmployeeForm from './components/pages/EmployeeForm.jsx';
 import EmployeeDetail from './components/pages/EmployeeDetail.jsx';
+import CompanyList from './components/pages/CompanyList';
 
 //router added at root level so in can be used in the entire application
 
@@ -105,6 +106,7 @@ function App() {
         <Route path="/employeelist" element={<EmployeeList />} />
         <Route path="/employeeform" element={<EmployeeForm />} />
         <Route path="/detail/:id" element={<EmployeeDetail />} />
+        <Route path="/companylist" element={<CompanyList />} />
       </Routes>
     </div>
   );
