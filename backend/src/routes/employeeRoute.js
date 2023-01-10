d8ad44fc73d09d321fd3db8f3a7e0f3581b3453c
Mariela <mvmihilova@gmail.com>
2023-01-10 15:23:51 -0700

    calling function emailNewEmployee

--- backend/src/routes/employeeRoute.js
+++ backend/src/routes/employeeRoute.js
@@ -1,70 +1,70 @@
 import express from 'express';
-import { Router } from 'express';
+import {Router} from 'express';
+import emailNewEmployee from '../../config/emailNewEmployee.js';
 import {
   createEmployee,
   deleteEmployee,
   getAllEmployees,
   getEmployeeById,
-  updateEmployee
+  updateEmployee,
 } from '../models/employeeModels.js';
 
-const router = Router();
+const router = Router ();
 
-router.post('/', async (req, res) => {
+router.post ('/', async (req, res) => {
   const employee = req.body;
   try {
-    const newEmployee = await createEmployee(employee);
-    res.send(newEmployee);
+    const newEmployee = await createEmployee (employee);
+    res.send (newEmployee);
+    emailNewEmployee (newEmployee);
   } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+    console.log (error);
+    res.status (500).send (error);
   }
 });
-router.get('/', async (req, res) => {
+router.get ('/', async (req, res) => {
   try {
-    const employees = await getAllEmployees();
-    res.send(employees);
+    const employees = await getAllEmployees ();
+    res.send (employees);
   } catch (error) {
-    res.status(500).send(error);
+    res.status (500).send (error);
   }
 });
 
 //identify an employee by ID
-router.get('/:id', async (req, res) => {
+router.get ('/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const employee = await getEmployeeById(id);
+    const employee = await getEmployeeById (id);
     if (!employee) {
-      return res.status(404).send('Invalid employee id');
+      return res.status (404).send ('Invalid employee id');
     }
-    res.send(employee);
+    res.send (employee);
   } catch (error) {
-    res.status(500).send(error);
+    res.status (500).send (error);
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put ('/:id', async (req, res) => {
   const id = req.params.id;
   const employeeValues = req.body;
   try {
-    const updatedEmployee = await updateEmployee(id, employeeValues);
-    console.log('updatedEmployee is', updateEmployee);
-    res.send(updatedEmployee);
+    const updatedEmployee = await updateEmployee (id, employeeValues);
+    console.log ('updatedEmployee is', updateEmployee);
+    res.send (updatedEmployee);
   } catch (error) {
-    res.status(500).send(error);
+    res.status (500).send (error);
   }
 });
 
-
-
-router.delete('/:id', async (req, res) => {
+router.delete ('/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    const deletedEmployee = await deleteEmployee(id);
-    console.log('deletedEmployee is', deleteEmployee);
-    res.send(deletedEmployee);
+    const deletedEmployee = await deleteEmployee (id);
+    console.log ('deletedEmployee is', deleteEmployee);
+    res.send (deletedEmployee);
   } catch (error) {
-    res.status(500).send(error);
+    res.status (500).send (error);
   }
 });
 export default router;
