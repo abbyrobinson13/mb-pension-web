88f3ccb46cef9d7f4a8b5d51bf7b954258ce2856
Mariela <mvmihilova@gmail.com>
2023-01-10 15:23:02 -0700

    formatting

--- backend/src/models/employeeModels.js
+++ backend/src/models/employeeModels.js
@@ -1,56 +1,58 @@
 import mongoose from 'mongoose';
 
-const employeeSchema = new mongoose.Schema({
+const employeeSchema = new mongoose.Schema ({
   firstName: {
-    type: String
+    type: String,
   },
   lastName: {
-    type: String
+    type: String,
   },
   gender: {
-    type: String
+    type: String,
   },
   dateOfBirth: {
-    type: String
+    type: String,
   },
 
   email: {
     type: String,
     required: [true, 'Please add an email'],
-    unique: true
-  }
+    unique: true,
+  },
 });
 
-export const Employee = mongoose.model('Employee', employeeSchema);
+export const Employee = mongoose.model ('Employee', employeeSchema);
 
 // create employee
-export const createEmployee = async (employee) => {
-  const newEmployee = await Employee.create(employee);
+export const createEmployee = async employee => {
+  const newEmployee = await Employee.create (employee);
   return newEmployee;
 };
 
 //list all employees
 export const getAllEmployees = async () => {
-  const employees = await Employee.find();
+  const employees = await Employee.find ();
   return employees;
 };
 
 //get an employee by Id
-export const getEmployeeById = async (id) => {
-  console.log('trying to get employee', id);
-  const employee = await Employee.findById(id);
+export const getEmployeeById = async id => {
+  console.log ('trying to get employee', id);
+  const employee = await Employee.findById (id);
   return employee;
 };
 
 //edit/update employee
 export const updateEmployee = async (id, employeeValues) => {
-  const updatedEmployee = await Employee.findOneAndUpdate(
-    {_id: id}, employeeValues);
+  const updatedEmployee = await Employee.findOneAndUpdate (
+    {_id: id},
+    employeeValues
+  );
   return updatedEmployee;
 };
 
 //delete an employee
-export const deleteEmployee = async (id) => {
-  const deletedEmployee = await Employee.findByIdAndDelete(id);
+export const deleteEmployee = async id => {
+  const deletedEmployee = await Employee.findByIdAndDelete (id);
   return deletedEmployee;
 };
