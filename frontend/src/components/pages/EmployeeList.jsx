3d859f5aa10c19b534a7d8acc7ac6a68b6eca66b
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-18 09:19:38 -0700

    feat: added employee and policy number to table

--- frontend/src/components/pages/EmployeeList.jsx
+++ frontend/src/components/pages/EmployeeList.jsx
@@ -11,7 +11,7 @@ const EmployeeList = () => {
   console.log('id is', id);
 
   const handleDelete = async (id) => {
-    if (window.confirm(`Are sure you want to delete ?`)) {
+    if (window.confirm(`Are you sure you want to delete ?`)) {
       const response = await fetch(`/api/employee/${id}`, {
         method: 'DELETE'
       });
@@ -51,23 +51,27 @@ const EmployeeList = () => {
       <Table striped bordered hover>
         <thead>
           <tr>
+            <th>Employee Number</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Gender</th>
             <th>Date Of Birth</th>
             <th>Email</th>
+            <th>Policy Number</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {employees.map((employee) => {
             return (
-              <tr key={employee.firstName}>
+              <tr key={employee.employeeNumber}>
+                <td>{employee.employeeNumber} </td>
                 <td>{employee.firstName} </td>
                 <td>{employee.lastName} </td>
                 <td>{employee.gender} </td>
                 <td>{employee.dateOfBirth} </td>
                 <td>{employee.email} </td>
+                <td>{employee.policyNumber} </td>
                 <td>
                   <Link to={`/update/${employee._id}`}>
                     <Button
@@ -110,78 +114,4 @@ const EmployeeList = () => {
 };
 export default EmployeeList;
 
-// const EmployeeDetail = (props) => {
-//   const employee = props.employee;
-//   const { firstName, lastName, gender, dateOfBirth, email } = employee;
-//   return (
-//     <div>
-//       <p>First Name:</p>
-//       <p>{firstName}</p>
-//       <p>Last Name:</p>
-//       <p>{lastName}</p>
-//       <p>Gender:</p>
-//       <p>{gender}</p>
-//       <p>Date Of Birth</p>
-//       <p>{dateOfBirth}</p>
-//       <p>Email:</p>
-//       <p>{email}</p>
-//     </div>
-//   );
-// };
-// const EmployeeList = () => {
-//   const [employees, setEmployees] = useState([]);
 
-//   useEffect(() => {
-//     const getEmployees = async () => {
-//       const response = await fetch('/api/employee');
-//       const employeesData = await response.json();
-//       setEmployees(employeesData);
-//     };
-//     getEmployees();
-//   }, []);
-//   return (
-//     <div>
-//       <nav className="bg-dark navbar-dark navbar">
-//         <div className="row col-12 d-flex justify-content-center text-white">
-//           <h2>List Of Employees</h2>
-//         </div>
-//       </nav>
-//       <TableContainer component={Paper}>
-//         <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//           <TableHead>
-//             <TableRow>
-//               <TableCell>First Name</TableCell>
-//               <TableCell>Last Name</TableCell>
-//               <TableCell>Gender</TableCell>
-//               <TableCell>Date Of Birth</TableCell>
-//               <TableCell align="right">Email</TableCell>
-//               <TableCell align="right">Action</TableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {employees.map((employee) => (
-//               <TableRow
-//                 key={employee.firstName}
-//                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//               >
-//                 <TableCell component="th" scope="row">
-//                   {employee.firstName}
-//                 </TableCell>
-//                 <TableCell>{employee.lastName}</TableCell>
-//                 <TableCell>{employee.gender}</TableCell>
-//                 <TableCell>{employee.dateOfBirth}</TableCell>
-//                 <TableCell>{employee.department}</TableCell>
-//                 <TableCell align="left">{employee.email}</TableCell>
-//                 <TableCell align="left">
-//                   <Link to={`/detail/${employee._id}`}>Go to Detail</Link>
-//                 </TableCell>
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//     </div>
-//   );
-// };
-
-// export default EmployeeList;
