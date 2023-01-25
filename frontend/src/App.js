7516071148aa194822764a8777427812ea162d27
Abigail Robinson <abbyrobinson@shaw.ca>
2023-01-25 13:58:42 -0700

    edited routing, and added new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import './App.css';
-import Form from './components/common/Form.js';
-import { app } from './firebase-config.js';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate
-} from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
-} from 'firebase/auth';
-import Home from './components/Home.jsx';
-import { ToastContainer, toast } from 'react-toastify';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import WelcomePage from './components/WelcomePage.jsx';
 import Employees from './components/pages/employees.jsx';
@@ -28,91 +13,23 @@ import AdminHome from './components/AdminHome.jsx';
 import CompanyHome from './components/CompanyHome.jsx';
 import CompanyList from './components/CompanyList.jsx';
 import { AuthProvider } from './AuthProvider.js';
+import Login from './components/Login.jsx';
+import SignUp from './components/SignUp.jsx';
+import Profile from './components/Profile.jsx';
+import EditProfile from './components/EditProfile.jsx';
 
 //router added at root level so in can be used in the entire application
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  let navigate = useNavigate();
-
-  useEffect(() => {
-    let authToken = sessionStorage.getItem('Auth Token');
-
-    if (authToken) {
-      navigate('/home');
-    }
-  }, []);
-
-  const handleAction = (id) => {
-    const authentication = getAuth(app);
-    if (id === 1) {
-      signInWithEmailAndPassword(authentication, email, password)
-        .then((response) => {
-          navigate('/home');
-          sessionStorage.setItem(
-            'Auth Token',
-            response._tokenResponse.refreshToken
-          );
-        })
-        .catch((error) => {
-          if (error.code === 'auth/wrong-password') {
-            toast.error('Please check the Password');
-          }
-          if (error.code === 'auth/user-not-found') {
-            toast.error('Please check the Email');
-          }
-        });
-    }
-    if (id === 2) {
-      createUserWithEmailAndPassword(authentication, email, password)
-        .then((response) => {
-          navigate('/home');
-          sessionStorage.setItem(
-            'Auth Token',
-            response._tokenResponse.refreshToken
-          );
-        })
-        .catch((error) => {
-          console.log(error);
-          if (error.code === 'auth/email-already-in-use') {
-            toast.error('Email Already in Use');
-          }
-        });
-    }
-  };
-
   return (
     <div className="App">
-      <ToastContainer />
       <AuthProvider>
         <Routes>
           <Route path="/employeelist" element={<EmployeeList />} />
           <Route path="/employeeform" element={<EmployeeForm />} />
           <Route path="/" element={<WelcomePage />} />
-          <Route
-            path="/login"
-            element={
-              <Form
-                title="Log In"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(1)}
-              />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Form
-                title="Register"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(2)}
-              />
-            }
-          />
-          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/employees" element={<Employees />} />
           <Route path="/employeelist" element={<EmployeeList />} />
           <Route path="/createnewemployee" element={<CreateEmployee />} />
@@ -123,6 +40,8 @@ function App() {
           <Route path="/adminhome" element={<AdminHome />} />
           <Route path="/companyhome" element={<CompanyHome />} />
           <Route path="/companies" element={<CompanyList />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/editprofile" element={<EditProfile />} />
         </Routes>
       </AuthProvider>
     </div>
