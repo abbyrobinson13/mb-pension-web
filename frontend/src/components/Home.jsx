6d99709b90f2253efa816522c49942a03355a9fa
Abigail Robinson <abbyrobinson@shaw.ca>
2023-01-25 13:59:30 -0700

    commited out old home page, unsure if still needed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { NavBar } from './NavBar.jsx';
+// import React, { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { NavBar } from './NavBar.jsx';
 
-export default function Home() {
-  const handleLogout = () => {
-    sessionStorage.removeItem('Auth Token');
-    navigate('/');
-  };
-  let navigate = useNavigate();
-  useEffect(() => {
-    let authToken = sessionStorage.getItem('Auth Token');
-    console.log(authToken);
-    if (authToken) {
-      navigate('/home');
-    }
+// export default function Home() {
+//   const handleLogout = () => {
+//     sessionStorage.removeItem('Auth Token');
+//     navigate('/');
+//   };
+//   let navigate = useNavigate();
+//   useEffect(() => {
+//     let authToken = sessionStorage.getItem('Auth Token');
+//     console.log(authToken);
+//     if (authToken) {
+//       navigate('/home');
+//     }
 
-    if (!authToken) {
-      navigate('/register');
-    }
-  }, []);
-  return (
-    <div>
-      <NavBar />
-      <h1>Home Page</h1>
-      <button onClick={handleLogout}>Log out</button>
-    </div>
-  );
-}
+//     if (!authToken) {
+//       navigate('/register');
+//     }
+//   }, []);
+//   return (
+//     <div>
+//       <NavBar />
+//       <h1>Home Page</h1>
+//       <button onClick={handleLogout}>Log out</button>
+//     </div>
+//   );
+// }
