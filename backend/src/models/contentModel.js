72e8953a23e2cc31e29ac116bf81742ae2f287eb
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-06 08:50:50 -0700

    created schema for content

---
+++ backend/src/models/contentModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const contentSchema = new mongoose.Schema({
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  img: {
+    type: String
+  },
+  url: {
+    type: String
+  },
+  tags: {
+    type: Array
+  },
+  category: {
+    type: String
+  }
+});
+
+export const Content = mongoose.model('Content', contentSchema);
+
+//READ
+export const getAllContent = async () => {
+  const allContent = await Content.find();
+  return allContent;
+};
+
+//CREATE
+export const createContent = async (content) => {
+  const newContent = await Content.create(content);
+  return newContent;
+};
