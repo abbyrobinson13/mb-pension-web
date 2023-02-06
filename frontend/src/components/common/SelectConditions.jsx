6d7555528f4dacfc43ff4f81e13434d5090fdd78
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-06 08:53:28 -0700

    new component - select conditions

---
+++ frontend/src/components/common/SelectConditions.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SelectConditions() {
+  return <div>SelectConditions</div>;
+}
+
+const conditions = [
+  { title: 'Anxiety' },
+  { title: 'Addictions' },
+  { title: 'Insomnia' },
+  { title: 'Depression' },
+  { title: 'ADHD' },
+  { title: 'Eating Disorders ' },
+  { title: 'Trauma' },
+  { title: 'Emotional Regulation Problems' }
+];
+
+export default SelectConditions;
