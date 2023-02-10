2ba1286907faa8cc9140a16955e8ae32c9797537
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-10 10:00:36 -0700

    fixed create company

--- frontend/src/components/CreateCompany.jsx
+++ frontend/src/components/CreateCompany.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useRef } from 'react';
-import { signUp } from '../firebase-config.js';
-import { signUpBroker } from '../firebase-config.js';
+import { signUp, db, logOut } from '../firebase-config.js';
 import { Link } from 'react-router-dom';
 import { Card, Button, Form } from 'react-bootstrap';
 
@@ -24,17 +23,17 @@ function CreateCompany() {
     }
   };
 
-  const handleSubmitBroker = async (e) => {
-    e.preventDefault();
-    if (password !== password) {
-      seterror('Passwords do not match');
-    } else {
-      setEmail('');
-      setPassword('');
-      const res = await signUpBroker(email, password);
-      if (res.error) seterror(res.error);
-    }
-  };
+  // const handleSubmitBroker = async (e) => {
+  //   e.preventDefault();
+  //   if (password !== password) {
+  //     seterror('Passwords do not match');
+  //   } else {
+  //     setEmail('');
+  //     setPassword('');
+  //     const res = await signUpBroker(email, password);
+  //     if (res.error) seterror(res.error);
+  //   }
+  // };
 
   return (
     <>
@@ -61,31 +60,34 @@ function CreateCompany() {
           <button type="submit">Submit</button>
         </form>
       </div>
-      <h2>Sign Up - New Broker</h2>
-      <div>
-        {error ? <div> {error} </div> : null}
-        <form onSubmit={handleSubmitBroker}>
-          <input
-            type="email"
-            name="email"
-            value={emailBroker}
-            placeholder="Your Email"
-            required
-            onChange={(e) => setEmailBroker(e.target.value)}
-          />
-          <input
-            type="password"
-            name="password"
-            value={passwordBroker}
-            placeholder="Your Password"
-            required
-            onChange={(e) => setPasswordBroker(e.target.value)}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
     </>
   );
 }
 
 export default CreateCompany;
+
+//Afsins create broker code
+{
+  /* <div>
+{error ? <div> {error} </div> : null}
+<form onSubmit={handleSubmitBroker}>
+  <input
+    type="email"
+    name="email"
+    value={emailBroker}
+    placeholder="Your Email"
+    required
+    onChange={(e) => setEmailBroker(e.target.value)}
+  />
+  <input
+    type="password"
+    name="password"
+    value={passwordBroker}
+    placeholder="Your Password"
+    required
+    onChange={(e) => setPasswordBroker(e.target.value)}
+  />
+  <button type="submit">Submit</button>
+</form>
+</div> */
+}
