5174ad16c051f5a82691b506c97ea8d95aa67877
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-10 10:42:06 -0700

    fixed post route

--- frontend/src/components/AddBenefits.jsx
+++ frontend/src/components/AddBenefits.jsx
@@ -1,24 +1,25 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BenefitsForm from '../shared/BenefitsForm';
 
 function AddBenefits({}) {
-  const [confirmSubmit, setConfirmSubmit] = useState (false);
-  const navigate = useNavigate ();
-  const handleSubmit = async benefits => {
-    alert ('Form submitted');
-    setConfirmSubmit (true);
-    navigate ('/');
+  const [confirmSubmit, setConfirmSubmit] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (benefits) => {
+    alert('Form submitted');
+    setConfirmSubmit(true);
+    navigate('/');
 
-    const response = await fetch ('/api/benefits', {
+    const response = await fetch('/api/benefits', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify (benefits),
+      body: JSON.stringify(benefits)
     });
-    const newBenefits = await response.json ();
-    console.log (newBenefits);
+    const newBenefits = await response.json();
+    console.log('benefits submitted');
+    console.log(newBenefits);
   };
   return (
     <div>
