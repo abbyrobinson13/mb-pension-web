c028b9f984fb84ec8af0e751df56ef2b82c7c0f6
Mariela <mvmihilova@gmail.com>
2023-02-10 13:41:58 -0700

    added handleSubmit

--- frontend/src/shared/BenefitsForm.jsx
+++ frontend/src/shared/BenefitsForm.jsx
@@ -1,48 +1,66 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Select from 'react-select';
 import Button from '@mui/material/Button';
 import '../App.css';
 
 //Does the client have paramedical benefits available?
 const paramedicalOptions = [
-  { value: 'Yes', label: 'Yes' },
-  { value: 'No', label: 'No' }
+  {value: 'Yes', label: 'Yes'},
+  {value: 'No', label: 'No'},
 ];
 const coinsuranceLevelOptions = {
   Yes: [
-    { value: '100', label: '100' },
-    { value: '90', label: '90' },
-    { value: '80', label: '80' },
-    { value: '70', label: '70' }
+    {value: '100', label: '100'},
+    {value: '90', label: '90'},
+    {value: '80', label: '80'},
+    {value: '70', label: '70'},
   ],
-  No: [{ value: 'N/A', label: 'N/A' }]
+  No: [{value: 'N/A', label: 'N/A'}],
 };
 
-const Benefits = ({ handleSubmit }) => {
-  const [selectedParamedical, setSelectedParamedical] = useState(null);
-  const [selectedCoinsuranceLevelOptions, setSelectedCoinsuranceLevel] =
-    useState(null);
+const BenefitsForm = () => {
+  const [selectedParamedical, setSelectedParamedical] = useState (null);
+  const [
+    selectedCoinsuranceLevelOptions,
+    setSelectedCoinsuranceLevel,
+  ] = useState (null);
 
-  const handleParamedicalChange = (selectedParamedicalOption) => {
-    setSelectedParamedical(selectedParamedicalOption);
-    console.log(selectedParamedicalOption);
+  const handleSubmit = async e => {
+    e.preventDefault ();
+    const benefits = {
+      selectedParamedical,
+      selectedCoinsuranceLevelOptions,
+    };
+
+    const response = await fetch ('/api/benefits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify (benefits),
+    });
+    const newBenefits = await response.json ();
+    console.log (newBenefits);
   };
 
-  const handleSelectedCoinsuranceLevelOptions = (
-    selectedCoinsuranceLevelOptions
-  ) => {
-    setSelectedCoinsuranceLevel(selectedCoinsuranceLevelOptions);
-    console.log(selectedCoinsuranceLevelOptions);
+  const handleParamedicalChange = selectedParamedicalOption => {
+    setSelectedParamedical (selectedParamedicalOption);
+    console.log (selectedParamedicalOption);
+  };
+
+  const handleSelectedCoinsuranceLevelOptions = selectedCoinsuranceLevelOptions => {
+    setSelectedCoinsuranceLevel (selectedCoinsuranceLevelOptions);
+    console.log (selectedCoinsuranceLevelOptions);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label style={{ display: 'flex', margin: 20 }}>
+      <label style={{display: 'flex', margin: 20}}>
         <h2>Benefit Booklet Form for Companies/Employees</h2>
       </label>
       {/* Does the client have paramedical benefits available? */}
       <div>
-        <div style={{ width: 600, marginBottom: 20, margin: 20 }}>
+        <div style={{width: 600, marginBottom: 20, margin: 20}}>
           <b>Does the client have paramedical benefits available?</b>
           <Select
             options={paramedicalOptions}
@@ -50,22 +68,21 @@ const Benefits = ({ handleSubmit }) => {
             value={selectedParamedical}
           />
         </div>
-        {selectedParamedical && (
-          <div style={{ width: 600, marginBottom: 20, margin: 20 }}>
+        {selectedParamedical &&
+          <div style={{width: 600, marginBottom: 20, margin: 20}}>
             <b>What is the coinsurance level (%)?</b>
             <Select
               options={coinsuranceLevelOptions[selectedParamedical.value]}
               onChange={handleSelectedCoinsuranceLevelOptions}
               value={selectedCoinsuranceLevelOptions}
             />
-          </div>
-        )}
+          </div>}
       </div>
 
       <Button
         type="submit"
         variant="contained"
-        style={{ display: 'flex', margin: 20, width: 200 }}
+        style={{display: 'flex', margin: 20, width: 200}}
       >
         SUBMIT
       </Button>
@@ -73,4 +90,4 @@ const Benefits = ({ handleSubmit }) => {
   );
 };
 
-export default Benefits;
+export default BenefitsForm;
