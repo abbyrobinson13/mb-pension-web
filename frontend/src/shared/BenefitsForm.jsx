493ba292de9dd90d3fe91100649f3a03f6a997a3
Mariela <mvmihilova@gmail.com>
2023-02-09 21:49:48 -0700

    added handleSubmit

--- frontend/src/shared/BenefitsForm.jsx
+++ frontend/src/shared/BenefitsForm.jsx
@@ -25,37 +25,56 @@ const Benefits = () => {
     setSelectedParamedical (selectedParamedicalOption);
   };
 
-  return (
-    <form>
-      <label style={{display: 'flex', margin: 20}}>
-        <h2>Benefit Booklet Form for Companies/Employees</h2>
-      </label>
-      {/* Does the client have paramedical benefits available? */}
-      <div>
-        <div style={{width: 600, marginBottom: 20, margin: 20}}>
-          <b>Does the client have paramedical benefits available?</b>
-          <Select
-            options={paramedicalOptions}
-            onChange={handleParamedicalChange}
-            value={selectedParamedical}
-          />
-        </div>
-        {selectedParamedical &&
-          <div style={{width: 600, marginBottom: 20, margin: 20}}>
-            <b>What is the coinsurance level (%)?</b>
-            <Select
-              options={coinsuranceLevelOptions[selectedParamedical.value]}
-            />
-          </div>}
-      </div>
+  const handleSubmit = async e => {
+    e.preventDefault ();
+    const benefits = {
+      selectedParamedical,
+    };
+    const response = await fetch ('/api/benefits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify (benefits),
+    });
+    const newBenefits = await response.json ();
+    console.log (newBenefits);
+  };
+  console.log (selectedParamedical);
 
-      <Button
-        variant="contained"
-        style={{display: 'flex', margin: 20, width: 200}}
-      >
-        SUBMIT
-      </Button>
-    </form>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label style={{display: 'flex', margin: 20}}>
+          <h2>Benefit Booklet Form for Companies/Employees</h2>
+        </label>
+        {/* Does the client have paramedical benefits available? */}
+        <div>
+          <div style={{width: 600, marginBottom: 20, margin: 20}}>
+            <b>Does the client have paramedical benefits available?</b>
+            <Select
+              options={paramedicalOptions}
+              onChange={handleParamedicalChange}
+              value={selectedParamedical}
+            />
+          </div>
+          {selectedParamedical &&
+            <div style={{width: 600, marginBottom: 20, margin: 20}}>
+              <b>What is the coinsurance level (%)?</b>
+              <Select
+                options={coinsuranceLevelOptions[selectedParamedical.value]}
+              />
+            </div>}
+        </div>
+
+        <Button
+          variant="contained"
+          style={{display: 'flex', margin: 20, width: 200}}
+        >
+          SUBMIT
+        </Button>
+      </form>
+    </div>
   );
 };
 
