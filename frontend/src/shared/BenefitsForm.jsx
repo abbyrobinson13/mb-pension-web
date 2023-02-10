9c1b38ab7f98d17c31c0ee70d325ab7a4326f014
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-10 10:42:25 -0700

    updated benefits form

--- frontend/src/shared/BenefitsForm.jsx
+++ frontend/src/shared/BenefitsForm.jsx
@@ -1,80 +1,75 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import Button from '@mui/material/Button';
 import '../App.css';
 
 //Does the client have paramedical benefits available?
 const paramedicalOptions = [
-  {value: 'Yes', label: 'Yes'},
-  {value: 'No', label: 'No'},
+  { value: 'Yes', label: 'Yes' },
+  { value: 'No', label: 'No' }
 ];
 const coinsuranceLevelOptions = {
   Yes: [
-    {value: '100', label: '100'},
-    {value: '90', label: '90'},
-    {value: '80', label: '80'},
-    {value: '70', label: '70'},
+    { value: '100', label: '100' },
+    { value: '90', label: '90' },
+    { value: '80', label: '80' },
+    { value: '70', label: '70' }
   ],
-  No: [{value: 'N/A', label: 'N/A'}],
+  No: [{ value: 'N/A', label: 'N/A' }]
 };
 
-const Benefits = () => {
-  const [selectedParamedical, setSelectedParamedical] = useState (null);
+const Benefits = ({ handleSubmit }) => {
+  const [selectedParamedical, setSelectedParamedical] = useState(null);
+  const [selectedCoinsuranceLevelOptions, setSelectedCoinsuranceLevel] =
+    useState(null);
 
-  const handleParamedicalChange = selectedParamedicalOption => {
-    setSelectedParamedical (selectedParamedicalOption);
+  const handleParamedicalChange = (selectedParamedicalOption) => {
+    setSelectedParamedical(selectedParamedicalOption);
+    console.log(selectedParamedicalOption);
   };
 
-  const handleSubmit = async e => {
-    e.preventDefault ();
-    const benefits = {
-      selectedParamedical,
-    };
-    const response = await fetch ('/api/benefits', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify (benefits),
-    });
-    const newBenefits = await response.json ();
-    console.log (newBenefits);
+  const handleSelectedCoinsuranceLevelOptions = (
+    selectedCoinsuranceLevelOptions
+  ) => {
+    setSelectedCoinsuranceLevel(selectedCoinsuranceLevelOptions);
+    console.log(selectedCoinsuranceLevelOptions);
   };
 
   return (
-
-      <form onSubmit={handleSubmit}>
-
-        <label style={{display: 'flex', margin: 20}}>
-          <h2>Benefit Booklet Form for Companies/Employees</h2>
-        </label>
-        {/* Does the client have paramedical benefits available? */}
-        <div>
-          <div style={{width: 600, marginBottom: 20, margin: 20}}>
-            <b>Does the client have paramedical benefits available?</b>
+    <form onSubmit={handleSubmit}>
+      <label style={{ display: 'flex', margin: 20 }}>
+        <h2>Benefit Booklet Form for Companies/Employees</h2>
+      </label>
+      {/* Does the client have paramedical benefits available? */}
+      <div>
+        <div style={{ width: 600, marginBottom: 20, margin: 20 }}>
+          <b>Does the client have paramedical benefits available?</b>
+          <Select
+            options={paramedicalOptions}
+            onChange={handleParamedicalChange}
+            value={selectedParamedical}
+          />
+        </div>
+        {selectedParamedical && (
+          <div style={{ width: 600, marginBottom: 20, margin: 20 }}>
+            <b>What is the coinsurance level (%)?</b>
             <Select
-              options={paramedicalOptions}
-              onChange={handleParamedicalChange}
-              value={selectedParamedical}
+              options={coinsuranceLevelOptions[selectedParamedical.value]}
+              onChange={handleSelectedCoinsuranceLevelOptions}
+              value={selectedCoinsuranceLevelOptions}
             />
           </div>
-          {selectedParamedical &&
-            <div style={{width: 600, marginBottom: 20, margin: 20}}>
-              <b>What is the coinsurance level (%)?</b>
-              <Select
-                options={coinsuranceLevelOptions[selectedParamedical.value]}
-              />
-            </div>}
-        </div>
+        )}
+      </div>
 
-        <Button
+      <Button
         type="submit"
-          variant="contained"
-          style={{display: 'flex', margin: 20, width: 200}}
-        >
-          SUBMIT
-        </Button>
-      </form>
+        variant="contained"
+        style={{ display: 'flex', margin: 20, width: 200 }}
+      >
+        SUBMIT
+      </Button>
+    </form>
   );
 };
 
