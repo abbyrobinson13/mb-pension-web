40cd171fd29cdde6e787c20ae4d1c42168acd36e
Mariela <mvmihilova@gmail.com>
2023-02-10 20:59:57 -0700

    type Object

--- backend/src/models/benefitsModel.js
+++ backend/src/models/benefitsModel.js
@@ -1,24 +1,28 @@
 //For Companies Benefits Form
 import mongoose from 'mongoose';
-const benefitsSchema = new mongoose.Schema({
-  selectedParamedical: {
+const benefitsSchema = new mongoose.Schema ({
+  companyName: {
     type: String,
-    required: false
+    required: false,
   },
-  selectedCoinsuranceLevelOptions: {
-    type: Number,
-    required: false
-  }
+  selectedParamedical: {
+    type: Object,
+    required: true,
+  },
+  selectedCoinsuranceLevel: {
+    type: Object,
+    required: false,
+  },
 });
 
-export const Benefits = mongoose.model('Benefits', benefitsSchema);
+export const Benefits = mongoose.model ('Benefits', benefitsSchema);
 
 export const getAllBenefits = async () => {
-  const allBenefits = await Benefits.find();
+  const allBenefits = await Benefits.find ();
   return allBenefits;
 };
 
-export const createBenefits = async (benefits) => {
-  const newBenefits = await Benefits.create(benefits);
+export const createBenefits = async benefits => {
+  const newBenefits = await Benefits.create (benefits);
   return newBenefits;
 };
