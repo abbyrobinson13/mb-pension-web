536097b8df7d6895a404482fa0567903a9a33cf9
Mariela <mvmihilova@gmail.com>
2023-02-12 13:43:29 -0700

    console.log APIKey

--- frontend/src/shared/UploadForm.jsx
+++ frontend/src/shared/UploadForm.jsx
@@ -1,40 +1,46 @@
-import {useState} from 'react';
-import {storage} from '../firebase-config.js'
-import {ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage';
+import { useState } from 'react';
+import { storage } from '../firebase-config.js';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
-function UploadForm () {
+function UploadForm() {
   // State to store uploaded file
-  const [file, setFile] = useState (''); // progress
-  const [percent, setPercent] = useState (0); // Handle file upload event and update state
-  function handleChange (event) {
-    setFile (event.target.files[0]);
+  const [file, setFile] = useState(''); // progress
+  const [percent, setPercent] = useState(0); // Handle file upload event and update state
+  function handleChange(event) {
+    setFile(event.target.files[0]);
   }
   const handleUpload = () => {
     if (!file) {
-      alert ('Please upload a document first!');
+      alert('Please upload a document first!');
     }
-    const storageRef = ref (storage, `/files/${file.name}`); // progress can be paused and resumed. It also exposes progress updates. // Receives the storage reference and the file to upload.
-    const uploadTask = uploadBytesResumable (storageRef, file);
-    uploadTask.on (
+    const storageRef = ref(storage, `/files/${file.name}`); // progress can be paused and resumed. It also exposes progress updates. // Receives the storage reference and the file to upload.
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
       'state_changed',
-      snapshot => {
-        const percent = Math.round (
-          snapshot.bytesTransferred / snapshot.totalBytes * 100
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         ); // update progress
-        setPercent (percent);
+        console.log('setting percent to:', percent);
+        setPercent(percent);
       },
-      err => console.log (err),
+      (err) => console.log('upload task error', err),
       () => {
         // download url
-        getDownloadURL (uploadTask.snapshot.ref).then (url => {
-          console.log (url);
-        });
+        console.log('calling getDownloadURL()');
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((url) => {
+            console.log('get download URL got:', url);
+          })
+          .catch((err) => {
+            console.log('get download URL got an error!', err);
+          });
       }
     );
   };
   return (
     <div>
-      <input type="file" onChange={handleChange} accept="/pdf/*" />
+      <input type="file" onChange={handleChange} accept="/files/*" />
       <button onClick={handleUpload}>Upload to Firebase</button>
       <p>{percent} "% done"</p>
     </div>
