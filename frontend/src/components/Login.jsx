bb86b0470a0f45fb5d035f4c0ae89a5e8ee12f91
Mariela <mvmihilova@gmail.com>
2023-02-14 12:26:44 -0700

    added mui basic styling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { signIn } from '../firebase-config.js';
 import { useNavigate } from 'react-router-dom';
+import {Avatar} from '@mui/material';
+import { Button } from '@mui/material';
+import {TextField} from '@mui/material';
+import FormControlLabel from '@mui/material';
+import Typography from '@mui/material';
+import Box from '@mui/material/Box';
+import LockIcon from '@mui/icons-material/Lock';
+import {Grid} from '@mui/material';
+import {Paper} from '@mui/material';
+import '../App.css';
 
-function Login() {
+
+export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, seterror] = useState('');
-
+ 
+  
   let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -20,28 +32,62 @@ function Login() {
   };
 
   return (
-    <>
-      <h1>Log in</h1>
+    <Grid container component="main" className= "root-login">
+       <Grid
+        className= "size-grid-logins"
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={1}
+        square
+      >
+        <div className= "paper-logins">
+          <Avatar className= "avatar-logins">
+            <LockIcon />
+          </Avatar>
+      <h1>Sign In</h1>
       {error ? <div>{error}</div> : null}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="email"
+      <form className= "form-logins" noValidate onSubmit={handleSubmit}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="E-mail"
+          name="username"
+          autoFocus
           value={email}
-          placeholder="Your Email"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
-          type="password"
-          name="password"
+        <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
           value={password}
-          placeholder="Your Password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit"> Submit </button>
+        <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+            >
+              Sign In
+            </Button>
       </form>
-    </>
+    </div>
+      </Grid>
+    </Grid>
   );
 }
 
-export default Login;
+
