4a365f629783dd07a1157170598eff62b14bece4
Mariela <mvmihilova@gmail.com>
2023-02-16 21:23:16 -0700

    updated

--- frontend/src/components/CompanyHome.jsx
+++ frontend/src/components/CompanyHome.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import CompanyNavBar from './CompanyNavBar';
+import { NavLink } from 'react-router-dom';
+
 
 function CompanyHome() {
   return (
-    <div>
-      <CompanyNavBar />
-      <h1> Company Home Page </h1>
+    <div className="Admin-Container">
+    <nav>
+      <h1>Company Dashboard</h1>
+      </nav>
+      <div className="Button-Container" >
+        <NavLink to="/addbenefits" >
+      <img src="/images/ViewCompany.jpg" alt="Image 1" />
+          <span>Benefits Form for Employees</span>
+        </NavLink>
+        <NavLink to="/upload">
+        <img src="/images/Add.jpg" alt="Image 2" />
+          <span> Upload Document </span>
+        </NavLink>
+        <NavLink to="/employeelist">
+           <img src="/images/AddContent.jpg"  alt="Image 3" />
+          <span> Add Employee/ Display </span>
+        </NavLink>      
+      </div>
     </div>
+    // <div>
+    //   <CompanyNavBar />
+    //   <h1> Company Home Page </h1>
+    // </div>
   );
 }
 
