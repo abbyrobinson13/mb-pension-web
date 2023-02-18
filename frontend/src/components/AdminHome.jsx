854bfaa078873ba0a5f7d746302759f077d74397
Mariela <mvmihilova@gmail.com>
2023-02-17 21:26:43 -0700

    removed photos, changed colors

--- frontend/src/components/AdminHome.jsx
+++ frontend/src/components/AdminHome.jsx
@@ -29,15 +29,15 @@ function AdminHome() {
       </nav>
       <div className="Button-Container" >
         <NavLink to="/companies" >
-      <img src="/images/ViewCompany.jpg" alt="Image 1" />
+      {/* <img src="/images/ViewCompany.jpg" alt="Image 1" /> */}
           <span>View All Companies</span>
         </NavLink>
         <NavLink to="/createcompany">
-        <img src="/images/Add.jpg" alt="Image 2" />
+        {/* <img src="/images/Add.jpg" alt="Image 2" /> */}
           <span> Add New Company/Broker </span>
         </NavLink>
         <NavLink to="/addcontent">
-           <img src="/images/AddContent.jpg"  alt="Image 3" />
+           {/* <img src="/images/AddContent.jpg"  alt="Image 3" /> */}
           <span> Add Content </span>
         </NavLink>
         
