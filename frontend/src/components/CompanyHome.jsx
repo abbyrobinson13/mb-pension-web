e6b052861866956efa5e3f73e70b830bdcb829ae
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-22 07:31:06 -0700

    updated company home

--- frontend/src/components/CompanyHome.jsx
+++ frontend/src/components/CompanyHome.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import CompanyNavBar from './CompanyNavBar';
 import { NavLink } from 'react-router-dom';
 
-
 function CompanyHome() {
   return (
     <div className="Admin-Container">
-    <nav>
-      <h1>Company Dashboard</h1>
+      <nav>
+        <h1>Company Dashboard</h1>
       </nav>
-      <div className="Button-Container" >
-        <NavLink to="/addbenefits" >
-      <img src="../images/notebook-plant.jpg" alt="notebook pencils and plant" />
+      <div className="Button-Container">
+        <NavLink to="/addbenefits">
+          <img
+            src="../images/notebook-plant.jpg"
+            alt="notebook pencils and plant"
+          />
           <span>Benefits Form</span>
         </NavLink>
         <NavLink to="/upload">
-        <img src="/images/screen-upload.jpg" alt="Image 2" />
+          <img src="/images/screen-upload.jpg" alt="Image 2" />
           <span> Upload a Document </span>
         </NavLink>
         <NavLink to="/employeelist">
-           <img src="/images/new-hired.jpg"  alt="Image 3" />
-          <span> Add Employee/ Display </span>
-        </NavLink>      
+          <img src="/images/new-hired.jpg" alt="Image 3" />
+          <span> View Employees </span>
+        </NavLink>
       </div>
     </div>
     // <div>
